16b685aa34b86021feca8495288fc0d5191453ea
Harmdhast <pandemonium.harmdhast@gmail.com>
2024-05-14 02:19:56 +0200

    add logo

--- app/src/main/java/com/example/unwired_android/Group.kt
+++ app/src/main/java/com/example/unwired_android/Group.kt
@@ -9,6 +9,7 @@ import androidx.activity.viewModels
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -29,6 +30,7 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -40,6 +42,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -95,6 +98,14 @@ fun Group(id: Int, viewModel: GroupViewModel) {
     }
 
     Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Image(
+                    painter = painterResource(R.drawable.unwired_white),
+                    "Logo"
+                )
+            })
+        },
         bottomBar = {
             NavigationBar {
 
